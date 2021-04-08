51606cb74e5c93161b835eda13386aa330f510f6
Elizabeth Wang <elizabethwang@lizzywangs-mbp.myfiosgateway.com>
2021-04-08 14:01:40 -0400

    model

--- code/models.py
+++ code/models.py
@@ -22,13 +22,6 @@ class SimpleModel(tf.keras.Model):
        ## kernel_size: An integer or tuple/list of 2 integers, specifying the height and width of the 2D convolution window. Can be a single integer to specify the same value for all spatial dimensions.
        ## strides: An integer or tuple/list of 2 integers, specifying the strides of the convolution along the height and width. Can be a single integer to specify the same value for all spatial dimensions. Specifying any stride value != 1 is incompatible with specifying any dilation_rate value != 1.
         self.architecture = [
-<<<<<<< Updated upstream
-            Conv2D(64, 48, 48, padding="same", activation="relu"),
-            Conv2D(124, 24, 24, padding="same", activation="relu"),
-            MaxPool2D(2),  
-            Conv2D(64, 3, 1, padding="same", activation="relu"),
-=======
->>>>>>> Stashed changes
             Conv2D(64, 3, 1, padding="same", activation="relu"),
             BatchNormalization(),
             Dropout(0.20),
