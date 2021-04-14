e8a9907b8016554b658b7cb305567f99dee915fb
Natalie rhsiadat <natalie_rshaidat@brown.edu>
2021-04-14 10:46:22 -0400

    Added option to use less filters in augmentation

---
+++ .history/code/datasetup_20210414094730.py
@@ -0,0 +1,114 @@
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import pandas as pd
+import cv2
+import os
+import glob
+from pathlib import Path
+
+def cleanTestDirs():
+    for f in Path('/Users/Natalie/Desktop/cs1430/CV-final-project/data/test').glob('*.jpg'):
+        try:
+            f.unlink()
+        except OSError as e:
+            print("Error: %s : %s" % (f, e.strerror))
+def cleanTrainDirs():
+    for f in Path('/Users/Natalie/Desktop/cs1430/CV-final-project/data/train').glob('*.jpg'):
+        try:
+            f.unlink()
+        except OSError as e:
+            print("Error: %s : %s" % (f, e.strerror))
+def cleanAll():
+    cleanTestDirs()
+    cleanTrainDirs()
+def createPixelArray(arr):
+  arr = list(map(int, arr.split()))
+  array = np.array(arr, dtype=np.uint8)
+  array = array.reshape((48, 48))
+  return array
+def equalize_hist(img):
+  img = cv2.equalizeHist(img)
+  return img
+def showImages(imgs):
+  _, axs = plt.subplots(1, len(imgs), figsize=(20, 20))
+  axs = axs.flatten()
+  for img, ax in zip(imgs, axs):
+    ax.imshow(img,cmap=plt.get_cmap('gray'))
+  plt.show()
+def augmentIMG(img, task):
+    imgs = [img]
+    img1 = equalize_hist(img)
+    imgs.append(img1)
+    if(task == 3):
+        img2 = cv2.bilateralFilter(img1, d=9, sigmaColor=75, sigmaSpace=75)
+        imgs.append(img2)
+    img6 = cv2.flip(img, 1) # flip horizontally
+    imgs.append(img6)
+    return imgs
+def saveIMG(arr, num, folderLoc):
+    im = Image.fromarray(arr)
+    filename = folderLoc + "image_"+ num+".jpg"
+    im.save(filename)
+def createTrain(emotion_dict, task):
+    df = pd.read_csv('/Users/elizabethwang/Desktop/CS1430/CV-final-project/data/train.csv') # CHANGE ME 
+    base_filename = '/Users/elizabethwang/Desktop/CS1430/CV-final-project/data/train/' # CHANGE ME
+    for index, row in df.iterrows():
+        if (row[' Usage'] == "Training"):
+            px = row[' pixels']
+            emot = int(row['emotion'])
+            emot_loc = emotion_dict[emot]
+            filename = base_filename + emot_loc
+            img = createPixelArray(px)
+            img_arr = augmentIMG(img, task)
+            idx = 0
+            for i in img_arr:
+                num = str(index) + "_" + str(idx)
+                idx +=1
+                saveIMG(i, num, filename)
+def createTest(emotion_dict , task):
+    df = pd.read_csv('/Users/elizabethwang/Desktop/CS1430/CV-final-project/data/icml_face_data.csv') # CHANGE ME
+    base_filename = "/Users/elizabethwang/Desktop/CS1430/CV-final-project/data/test/" # CHANGE ME 
+    for index, row in df.iterrows():
+        if (row[' Usage'] == "PublicTest"):
+            px = row[' pixels']
+            emot = int(row['emotion'])
+            emot_loc = emotion_dict[emot]
+            filename = base_filename + emot_loc
+            img = createPixelArray(px)
+            img_arr = augmentIMG(img, task)
+            idx = 0
+            for i in img_arr:
+                num = str(index) + "_" + str(idx)
+                idx +=1
+                saveIMG(i, num, filename)
+def createEmotionDict():
+    emotionDict = {}
+    emotionDict[0]="angry/"
+    emotionDict[1]="disgust/"
+    emotionDict[2]="fear/"
+    emotionDict[3]="happy/"
+    emotionDict[4]="sad/"
+    emotionDict[5]="surprise/"
+    emotionDict[6] = "neutral/"
+    return emotionDict
+def createSimpleData():
+    cleanAll()
+    emot_dict = createEmotionDict()
+    createTrain(emot_dict, 1)
+    createTest(emot_dict, 1)
+def createComplexData():
+    cleanAll()
+    emot_dict = createEmotionDict()
+    createTrain(emot_dict)
+    createTest(emot_dict)
+def main():
+    
+    emot_dict = createEmotionDict()
+    createTrain(emot_dict)
+    createTest(emot_dict)
+
+
+if __name__ == '__main__':
+    main()
---
+++ .history/code/preprocess_20210414095224.py
@@ -0,0 +1,165 @@
+
+import os
+import random
+import numpy as np
+from PIL import Image
+import tensorflow as tf
+
+import hyperparameters as hp
+from datasetup import createSimpleData, create
+
+class Datasets():
+    """ Class for containing the training and test sets as well as
+    other useful data-related information. Contains the functions
+    for preprocessing.
+    """
+
+    def __init__(self, data_path, task):
+
+        self.data_path = data_path
+        self.task = task
+        if task == 1:
+            
+
+        # Dictionaries for (label index) <--> (class name)
+        self.idx_to_class = {}
+        self.class_to_idx = {}
+
+        # For storing list of classes
+        self.classes = [""] * hp.num_classes
+
+        # Setup data generators
+        self.train_data = self.get_data(
+            os.path.join(self.data_path, "train/"), False)
+        self.test_data = self.get_data(
+            os.path.join(self.data_path, "test/"), False)
+
+    def calc_mean_and_std(self):
+        """ Calculate mean and standard deviation of a sample of the
+        training dataset for standardization.
+
+        Arguments: none
+
+        Returns: none
+        """
+
+        # Get list of all images in training directory
+        file_list = []
+        for root, _, files in os.walk(os.path.join(self.data_path, "train/")):
+            for name in files:
+                if name.endswith(".jpg"):
+                    file_list.append(os.path.join(root, name))
+
+        # Shuffle filepaths
+        random.shuffle(file_list)
+
+        # Take sample of file paths
+        file_list = file_list[:hp.preprocess_sample_size]
+
+        # Allocate space in memory for images
+        data_sample = np.zeros(
+            (hp.preprocess_sample_size, hp.img_size, hp.img_size, 3))
+
+        # Import images
+        for i, file_path in enumerate(file_list):
+            img = Image.open(file_path)
+            img = img.resize((hp.img_size, hp.img_size))
+            img = np.array(img, dtype=np.float32)
+            img /= 255.
+
+            # Grayscale -> RGB
+            if len(img.shape) == 2:
+                img = np.stack([img, img, img], axis=-1)
+
+            data_sample[i] = img
+
+        # Calculate the pixel-wise mean and standard deviation
+        # of the images in data_sample and store them in
+        # self.mean and self.std respectively.
+        # ==========================================================
+
+        self.mean = np.mean(data_sample, axis=(0,1,2))
+        self.std = np.std(data_sample, axis=(0,1,2))
+
+        # ==========================================================
+
+        print("Dataset mean: [{0:.4f}, {1:.4f}, {2:.4f}]".format(
+            self.mean[0], self.mean[1], self.mean[2]))
+
+        print("Dataset std: [{0:.4f}, {1:.4f}, {2:.4f}]".format(
+            self.std[0], self.std[1], self.std[2]))
+
+    def standardize(self, img):
+        """ Function for applying standardization to an input image.
+
+        Arguments:
+            img - numpy array of shape (image size, image size, 3)
+
+        Returns:
+            img - numpy array of shape (image size, image size, 3)
+        """
+
+        # Standardize the input image. Use self.mean and self.std
+        # that were calculated in calc_mean_and_std() to perform
+        # the standardization.
+        # =============================================================
+        img = img - self.mean
+        img = img/self.std
+
+        # =============================================================
+
+        return img
+
+    def preprocess_fn(self, img):
+        """ Preprocess function for ImageDataGenerator. """
+        img = img / 255.
+        return img
+
+    def get_data(self, path, shuffle):
+        """ Returns an image data generator which can be iterated
+        through for images and corresponding class labels.
+
+        Arguments:
+            path - Filepath of the data being imported, such as
+                   "../data/train" or "../data/test"
+            shuffle - Boolean value indicating whether the data should
+                      be randomly shuffled.
+
+        Returns:
+            An iterable image-batch generator
+        """
+
+        
+        data_gen = tf.keras.preprocessing.image.ImageDataGenerator(preprocessing_function=self.preprocess_fn)
+
+        # VGG must take images of size 224x224
+        img_size = hp.img_size
+
+        classes_for_flow = None
+
+        # Make sure all data generators are aligned in label indices
+        if bool(self.idx_to_class):
+            classes_for_flow = self.classes
+
+        # Form image data generator from directory structure
+        data_gen = data_gen.flow_from_directory(
+            path,
+            target_size=(img_size, img_size),
+            class_mode='sparse',
+            batch_size=hp.batch_size,
+            shuffle=shuffle,
+            classes=classes_for_flow)
+
+        # Setup the dictionaries if not already done
+        if not bool(self.idx_to_class):
+            unordered_classes = []
+            for dir_name in os.listdir(path):
+                if os.path.isdir(os.path.join(path, dir_name)):
+                    unordered_classes.append(dir_name)
+
+            for img_class in unordered_classes:
+                self.idx_to_class[data_gen.class_indices[img_class]] = img_class
+                self.class_to_idx[img_class] = int(data_gen.class_indices[img_class])
+                self.classes[int(data_gen.class_indices[img_class])] = img_class
+
+        return data_gen
